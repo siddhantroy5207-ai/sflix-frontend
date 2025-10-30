9462a00a4572bd9c31ddea69aca6bf01e46f66f1
siddhantroy5207-ai <siddhantroy5207@gmail.com>
2025-10-30 21:39:19 +0530

    Fix vite base path for vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// ✅ For Vercel Deployment
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
-});
+  base: './',
+})
